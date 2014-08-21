6b8d79991a7c261be0cba8c6c3ee80cde005513c
jhenriquez <julio.m.henriquez@gmail.com>
2014-08-21 19:18:42 -0400

    Applied the Dashboard layout to the World View for testing.

--- IronBank/IronBank/Controllers/HelloController.cs
+++ IronBank/IronBank/Controllers/HelloController.cs
@@ -8,11 +8,7 @@ namespace IronBank.Controllers
     {
         public ActionResult World()
         {
-            var db = new IronBank.Models.IronBankEntities();
-            var products = (from p in db.Products select p).ToList();
-            var productService = new ProductService();
-            productService.Save(new Product() { Type = ProductType.CreditCard, Balance = 500, Currency = ProductCurrency.Dollars });
-            return View(products);
+            return View();
         }
     }
 }
