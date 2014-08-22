7cdbc0a62350bdda8f100018d82932d30c5bcbf3
jhenriquez <julio.m.henriquez@gmail.com>
2014-08-21 20:55:28 -0400

    Added Home controller and login view. Still empty, tho...

---
+++ IronBank/IronBank/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace IronBank.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Ironbank: Login";
+            return View("Login");
+        }
+    }
+}
