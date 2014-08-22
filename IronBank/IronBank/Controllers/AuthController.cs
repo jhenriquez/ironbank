c8e606479e4782bca5fec12228008a9ad996ca6d
jhenriquez <julio.m.henriquez@gmail.com>
2014-08-22 00:45:32 -0400

    Added Auth and Dashboard Controllers.

---
+++ IronBank/IronBank/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using IronBank.Models;
+using System;
+using System.Security.Principal;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace IronBank.Controllers
+{
+    public class User : IPrincipal, IIdentity
+    {
+        public Int32 CustomerId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        public IIdentity Identity
+        {
+            get { return this; }
+        }
+
+        public bool IsInRole(string role)
+        {
+            return true;
+        }
+
+        public string AuthenticationType
+        {
+            get { return "batida"; }
+        }
+
+        public bool IsAuthenticated
+        {
+            get { return true; }
+        }
+
+        public string Name
+        {
+            get { return "Pablo"; }
+        }
+    }
+
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            Response.Cookies["Token"].Value = "343243243243243432432432";
+            Response.Cookies["Token"].Expires = DateTime.Now.AddHours(1);
+            return Redirect("/Dashboard/Index");
+        }
+    }
+}
---
+++ IronBank/IronBank/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace IronBank.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Token = Request.Cookies["Token"].Value;
+            return View("Summary");
+        }
+    }
+}
