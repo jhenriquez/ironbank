704cc69b6097a233e03be749403f997424b5db75
jhenriquez <julio.m.henriquez@gmail.com>
2014-08-27 19:59:40 -0400

    Added Controllers and Default Views For Menu Items.

---
+++ IronBank/IronBank/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace IronBank.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IronBank/IronBank/Controllers/TransfersController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace IronBank.Controllers
+{
+    public class TransfersController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
